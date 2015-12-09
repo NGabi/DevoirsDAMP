0db95d06b2d42baa741d5225312702ff96342944
NGabi <gabriela.nnitulescu@gmail.com>
2015-12-09 20:20:50 +0200

    dev 6 -services

--- Devoir6/Servicies/app/src/main/java/com/example/ibagn/MainActivity.java
+++ Devoir6/Servicies/app/src/main/java/com/example/ibagn/MainActivity.java
@@ -1,21 +1,42 @@
 package com.example.ibagn;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.widget.Button;
+import android.view.View;
+import android.widget.ImageButton;
 
 import com.example.ibagn.servicies.R;
 
 public class MainActivity extends AppCompatActivity {
-    Button start, end;
+    ImageButton start, end;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        start=(Button)findViewById(R.id.start);
-        end=(Button)findViewById(R.id.end);
-        start.onC
+        start=(ImageButton)findViewById(R.id.start);
+        end=(ImageButton)findViewById(R.id.end);
+        start.setOnClickListener(new View.OnClickListener(){
+            @Override
+            //On click function
+            public void onClick(View view) {
+                //Create the intent to start another activity
+                Intent inte =new Intent(getBaseContext(),MyService.class);
+                startService(inte);
+
+            }
+        });
+        end.setOnClickListener(new View.OnClickListener(){
+            @Override
+            //On click function
+            public void onClick(View view) {
+                //Create the intent to start another activity
+                Intent inte =new Intent(getBaseContext(),MyService.class);
+                startService(inte);
+
+            }
+        });
 
 
 
